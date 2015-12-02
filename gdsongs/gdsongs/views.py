5c15136174a686662d79a058a41958752842bb1c
maximinus <maximinus@gmail.com>
2015-12-02 13:01:30 +0800

    Started adding basic data checking to add shows page

--- gdsongs/gdsongs/views.py
+++ gdsongs/gdsongs/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.conf import settings
 from django.http import Http404
 
+from songs.models import Song
+
 def index(request):
 	"""Main landing page"""
 	return(render(request, 'index.html', {}))
 
 def addShow(request):
 	"""Complex page top add a show. Depends on a lot of AJAX calls"""
-	return(render(request, 'editing/add_show.html'))
+	songs = Song.objects.all()
+	return(render(request, 'editing/add_show.html', {'songs':songs}))
 
 # test functions follow below
 # these must all 404 on the production server
--- gdsongs/static/js/add_show.js
+++ gdsongs/static/js/add_show.js
@@ -1,12 +1,83 @@
 "use strict";
 
+function checkDigitString() {
+	// given a string 'XXX' ensure all digits and return the integer result
+	
+};
+
+function checkLength(text) {
+	// we expect a string of these formats, given d=some digit
+	// d or dd or d:dd or dd:dd
+	// split by ':' first
+	// if empty string, return as 0
+	if(text == '') {
+		return(0) }
+	var times = text.split(':');
+	if(times.length == 1) {
+		var minutes = 0;
+		var seconds = parseInt(times[0]);
+	}
+	else if(time.length == 2) {
+		var minutes = parseInt(time[0])
+		if((isNaN(minutes) == true) || (minutes < 0)) {
+			return(-1); }
+		var seconds = parseInt(times[1])
+	}
+	else {
+		// some error
+		return(-1) 
+	}
+	if((isNaN(seconds) == true) || (seconds > 59) || (seconds < 0)) {
+		return(-1)
+	}
+	// finally, return the real value
+	return((minutes * 60) + seconds);
+};
+
+function getRowData(row) {
+	// we have a <tr> instance
+	// the first 4 instances of <td> are, in order:
+	// the song, seque, length, comments
+	var cells = $(row).children();
+	// each cell has a child, we need to get that, and then the value of the input
+	// songs can never be wrong, they are just taken from the db
+	var song = $(cells[0]).children()[0].value;
+	// a seque can also never be wrong
+	var seque = $(cells[1]).children()[0].value;
+	// timings can be wrong
+	var len = $(cells[2]).children()[0].value;
+	seconds = checkLength(len);
+	if(seconds == -1) {
+		// highlight the error and then exit immediatly
+		return(null)
+	}
+	var comment = $(cells[3]).children()[0].value;
+	return([song, seque, len, comment]);
+};
+
+function verifyData() {
+	// so we have to check that things are ok
+	// firstly, we have to look through all the tabs
+	var tabs = ['#set-one', '#set-two', '#set-three', '#set-four']
+	var sets = [];
+	for(var i in tabs) {
+		var rows = $(tabs[i]).find('.data-row');
+		var rdata = [];
+		for(var j=0; j<rows.length; j++) {
+			rdata.push(getRowData(rows[j]))
+		}
+		sets.push(rdata);
+	}
+};
+
 function extractData() {
+	verifyData();
 };
 
 // functions below to handle the fron end
 
 function getRow() {
-	var html = '<tr><td><input list="songs" /></td>'
+	var html = '<tr class="data-row"><td><input list="songs" /></td>'
 	html += '<td><input type="checkbox" value="seque" /></td>'
 	html += '<td><input type="text" /></td>'
 	html += '<td><input type="text" /></td>'
@@ -85,5 +156,6 @@ $(document).ready(function() {
 	addSongs();
 	copyTableToTabs();
 	addCallbacks();
+	$('#post-data').click(extractData);
 });
 
