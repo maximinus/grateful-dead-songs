97c63203681d5bfaea48110b436ff2f59148f512
maximinus <maximinus@gmail.com>
2015-12-08 10:43:42 +0800

    Added checks for encores and dates on returning data

--- gdsongs/shows/views.py
+++ gdsongs/shows/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import ShowDate, Show, PlayedSet, PlayedSong
 from songs.models import Song
 
-import json
+import json, datetime
 
 class NewSet(object):
 	def __init__(songs, encore):
@@ -35,7 +35,14 @@ def uploadShow(request):
 	for i in sets:
 		json_data = json.loads(i)
 		songs = normalizeSetData(json_data)
-		if(songs == False):
+		date = normalizeDateData(date)
+		encore = normalizeEncoreData(encore)
+		
+		print songs
+		print date
+		print encore
+		
+		if((songs == False) or (date == False)):
 			return(HttpResponse(status=400))
 		set_data.append(songs)
 	return(HttpResponse(status=200))
@@ -70,4 +77,24 @@ def normalizeSetData(new_set):
 		normal_song.append(song[3])
 		normal.append(NewSong(*normal_song))
 	return(normal)
+
+def normalizeDateData(date):
+	# let's make a DB date object but not save it
+	# date is [day, month, year]
+	# and is -1 if unknown
+	for i in range(len(date)):
+		if(date[i] == -1):
+			date[i] == 0
+	if((date[0] != 0) and (date[1] != 0) and (date[2] != 0)):
+		# this should be a valid date, so test it
+		try:
+			valid = datetime(datetime(date[2], date[1], date[0]))
+		except ValueError:	
+			# invalid date
+			return(False)
+	# now the checks are over, the rest is easy
+	return(Showdate(year=date[2], month=date[1], day=date[0]))
+
+def normalizeEncoreData(encore):
+	return(json.loads(encore))
 
