f6e85ac475d963aa0da004044731b5abcfdbb69c
maximinus <maximinus@gmail.com>
2015-12-09 14:24:00 +0800

    Added base structure for venue selection

--- gdsongs/static/js/choose_venue.js
+++ gdsongs/static/js/choose_venue.js
@@ -1,12 +1,23 @@
 "use strict";
 
 function addDropdowns() {
-	for(var i=0; i<COUNTRIES.length; i++) {
-		$('#countries').append('<option value="' + COUNTRIES[i] + '">');
-	};
 	for(var i=0; i<STATES.length; i++) {
-		$('#states').append('<option value="' + STATES[i] + '">');
+		var option = '<option value"' + STATES[i] + '">' + STATES[i] + '</option>';
+		$('#state-select').append(option);
 	};
+};
+
+function countryChanged() {
+	// it's either TO the USA, in which case we turn on the state,
+	// or it's NOT the USA, in which case we turn ON the state.
+};
+
+function stateChanged() {
+	// call the DB to get all cites in this state
+};
+
+function cityChanged() {
+	// call the db to get all venues in this city and state
 };
 
 function addCallbacks() {
