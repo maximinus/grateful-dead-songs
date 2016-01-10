21dfbcb225873b761bfe99b3fd64affbcf84f90c
maximinus <maximinus@gmail.com>
2016-01-10 11:35:29 +0800

    Refactored the edit year page

--- gdsongs/static/js/edit_year.js
+++ gdsongs/static/js/edit_year.js
@@ -2,15 +2,19 @@
 
 function showSetList(e) {
 	// show the setlist and swap the button display
-	$(e.currentTarget).parent().children('.show-button').hide();
-	$(e.currentTarget).parent().children('.hide-button').show();
-	$(e.currentTarget).parent().children('.setlist').show();
+	var id = $(e.currentTarget).attr('id').split('-');
+	id = id[id.length - 1];
+	$('#show-button-' + id).hide();
+	$('#hide-button-' + id).show();
+	$('#setlist-' + id).show();
 };
 
 function hideSetList(e) {	
-	$(e.currentTarget).parent().children('.hide-button').hide();
-	$(e.currentTarget).parent().children('.setlist').hide();
-	$(e.currentTarget).parent().children('.show-button').show();
+	var id = $(e.currentTarget).attr('id').split('-');
+	id = id[id.length - 1];
+	$('#hide-button-' + id).hide();
+	$('#setlist-' + id).hide();
+	$('#show-button-' + id).show();
 };
 
 $(document).ready(function() {
