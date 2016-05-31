e59e1e512f018f930b6ea35fe4281bc62895697e
Maximinus <maximinus@gmail.com>
2016-05-31 13:39:26 +0100

    Tided up main front page

--- gdsongs/static/js/search.js
+++ gdsongs/static/js/search.js
@@ -1,9 +1,16 @@
 "use strict";
 
 function resizeWindow() {
-    var size = $('#navbar').outerHeight(true) + $('#center-content').outerHeight(true) + $('#vertical-fill').outerHeight(true);
-	var offset = (($(window).height() / 100) * 45) - size;
-	$('#vertical-fill').height(offset);
+	var size = $('#navbar').outerHeight(true) + $('#vertical-center').outerHeight(true) + $('#footer').outerHeight(true);
+	var offset = Math.floor(($(window).height() - size)/2);
+	if(offset < 1) {
+		return; }
+	console.log(offset);
+	$('#vertical-center').css('margin-top', offset);
+
+    //var size = $('#navbar').outerHeight(true) + $('#center-content').outerHeight(true) + $('#vertical-fill').outerHeight(true);
+	//var offset = (($(window).height() / 100) * 45) - size;
+	//$('#vertical-fill').height(offset);
 };
 
 function searchChange() {
@@ -16,9 +23,7 @@ function searchChange() {
 };
 
 $(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip();
     resizeWindow();
     $(window).resize(resizeWindow);
     $('#search-box').on('input', searchChange);
 });
-
