e3e6cc08ed0160f1a06dd0d8dde4caaccf5167c8
Maximinus <maximinus@gmail.com>
2016-06-02 11:12:41 +0100

    Fix for adding shows test

--- gdsongs/shows/views.py
+++ gdsongs/shows/views.py
@@ -127,9 +127,8 @@ def uploadShow(request):
 		venue = Venue.objects.get(pk=venue)
 	except:
 		return(sendError("Venue does not exist."))
-	#setlist = saveData(set_data, date, encore, venue)
-	#json_data = json.dumps({'msg':setlist})
-	json_data = json.dumps({'msg':'The show was ok'})
+	setlist = saveData(set_data, date, encore, venue)
+	json_data = json.dumps({'msg':setlist})
 	return(HttpResponse(json_data,  content_type='application/json', status=200))
 
 def normalizeSets(sets):
