b076ac8d511e442ac184d9a8e6eb418982f37eed
Maximinus <maximinus@gmail.com>
2016-06-13 14:40:57 +0100

    Unicode strings work for song names

--- gdsongs/shows/urls.py
+++ gdsongs/shows/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     url(r'^edit_shows/$', views.editShows, name='edit_shows'),
     url(r'^edit_shows/(?P<year>[0-9]+)/$', views.editYearShows, name='edit_shows_year'),
     url(r'^edit/(?P<show_id>[0-9]+)/$', views.editSingleShow, name='edit_single_show'),
-    url(r'^data/(?P<show_id>[0-9]+)/$', views.getShowAsJson, name='show_as_json'),
     url(r'^upload_edited_show/$', views.uploadEditedShow, name='upload_edited_show'),
     url(r'^year/(?P<year>[0-9]+)/$', views.getShowYears, name='get_show_years'),
     # testing
--- gdsongs/songs/management/commands/makeJSONsongs.py
+++ gdsongs/songs/management/commands/makeJSONsongs.py
@@ -1,7 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
 from songs.models import Song
 
-from song_list import songs
+import codecs
+
+# put file straight in the static files area
+# you'll need to do a ./manage.py collectstatic command after running this
+FILENAME = './static/js/songs.js'
 
 class Command(BaseCommand):
 	help = 'Adds all songs to database'
@@ -15,10 +19,11 @@ class Command(BaseCommand):
 			print('No songs in database')
 			return
 		
-		js_file = open('songs.js','w')
-		js_file.write('"use strict";\n\n')
-		js_file.write('var SONGS = [["{0}", {1}],\n'.format(songs[0].name, songs[0].id))
+		js_file = codecs.open(FILENAME,'w','utf-8')
+		js_file.write(u'"use strict";\n\n')
+		js_file.write(u'var SONGS = [["{0}", {1}],\n'.format(songs[0].name, songs[0].id))
 		for song in songs[1:len(songs) - 1]:
-			js_file.write('\t\t\t ["{0}", {1}],\n'.format(song.name, song.id))
-		js_file.write('\t\t\t ["{0}", {1}]];\n\n'.format(songs[len(songs) - 1].name, songs[len(songs) - 1].id))
+			js_file.write(u'\t\t\t ["{0}", {1}],\n'.format(song.name, song.id))
+		js_file.write(u'\t\t\t ["{0}", {1}]];\n\n'.format(songs[len(songs) - 1].name, songs[len(songs) - 1].id))
 		js_file.close()
+		print "songs.js file updated. You will need to collectstatic on the server"
