76cfcaf24a29dac5fbd67bddd06fc3180fb1d804
Maximinus <maximinus@gmail.com>
2016-06-14 20:59:57 +0100

    Added jquery-ui dialog

--- gdsongs/static/js/test_edit.js
+++ gdsongs/static/js/test_edit.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var DIALOG_TEMPLATE;
+
 function updateIndexOrder(event, ui) {
 	// we are not interested in the callback methods
 	// we just scan through every set and re-order internally if needed
@@ -16,16 +18,43 @@ function updateSet(set) {
 	});
 };
 
+function editSong() {
+	// render first and then show the dialog
+	$('#dialog').html(DIALOG_TEMPLATE({'title':'Edit song'}));
+	$('#dialog').dialog({
+		dialogClass: 'no-close',
+		draggable: false,
+		modal: true,
+		resizeable: false,
+		buttons: [
+			{text: 'Cancel',
+			 click: function() {
+			 	$(this).dialog('close'); },
+			},
+			{text: 'Save',
+			 click: function() {
+				$(this).dialog('close'); }
+			},
+		]
+	});
+};
+
 // add helper for songs index counting
 Handlebars.registerHelper('counter', function (index){
     return(index + 1);
 });
 
 $(document).ready(function() {
+	// prepare the dialog;
+	var template_data = $('#dialog-display').html();
+	DIALOG_TEMPLATE = Handlebars.compile(template_data);
 	// get the template html, compile it, add out context, put into DOM
-	var template_data = $('#show-display').html();
+	template_data = $('#show-display').html();
 	var show_template = Handlebars.compile(template_data);
 	var compiled_html = show_template(SHOW_DATA);
 	$('#show-show').html(compiled_html);
+	// setup the drag and drop for songs
 	$('.sortable').sortable({update: updateIndexOrder});
+	// allow double-clicking ona song
+	$('.song').dblclick(editSong);
 });
